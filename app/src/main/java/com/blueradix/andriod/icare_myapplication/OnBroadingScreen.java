35bca4abe66124bdd8f74acd9ecc25c02f9f78e4
Tiger <tiger.chalernphon15@gmail.com>
2021-07-27 14:53:06 +1000

    new commit

---
+++ app/src/main/java/com/blueradix/andriod/icare_myapplication/OnBroadingScreen.java
@@ -0,0 +1,14 @@
+package com.blueradix.andriod.icare_myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class OnBroadingScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_on_broading_screen);
+    }
+}
